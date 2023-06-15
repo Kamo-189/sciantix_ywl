8c5fa0c5e030a9ce50c7ae0002b3f04582834851
Charles Valot <charlesvalot@Charless-MacBook-Pro.local>
2023-06-15 16:09:52 +0200

    test

--- regression/regression.py
+++ regression/regression.py
@@ -44,6 +44,7 @@ def main():
     # (each test is executed with sciantix, and no modifications of gold)
     if os.environ.get('GITHUB_ACTIONS') == 'true':
         # Default values
+
         mode_gold = 0
         mode_plot = 1
         mode_Baker = 1
